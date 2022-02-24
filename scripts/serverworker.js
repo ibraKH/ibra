b4e8a34a0d90c7687b2da68391568c16fcb65c99
ibra <ibrakh80@gamail.com>
2022-02-24 12:33:07 +0300

    changes on sw

--- scripts/serverworker.js
+++ scripts/serverworker.js
@@ -1,5 +1,5 @@
 if("serviceWorker" in navigator){
-    navigator.serviceWorker.register("/scripts/sw.js").then(registration => {
+    navigator.serviceWorker.register("./sw.js").then(registration => {
       console.log("SW Registered!");
     }).catch(error => {
       console.log("SW Registration failed");
